f994c60c21e30f0077fb5371302efd2059e9475c
Costin Dragoi <costin@dragoi.me>
2021-02-19 00:08:56 +0100

    UPD

--- test/learn_cljs/test_runner.cljs
+++
@@ -1,9 +0,0 @@
-;; This test runner is intended to be run from the command line
-(ns learn-cljs.test-runner
-  (:require
-    ;; require all the namespaces that you want to test
-    [learn-cljs.weather-test]
-    [figwheel.main.testing :refer [run-tests-async]]))
-
-(defn -main [& args]
-  (run-tests-async 5000))
